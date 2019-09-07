199943a6b6e57f50051756031319c7397d3e6993
omersk <omersk@walla.co.il>
2019-09-07 22:47:05 +0300

    Better Results + Average Prediction Score
    This reverts commit a36552cd2b25e81c963f1b55be5f0aafa55b06dd.
    
    # Conflicts:
    #	FinalProject/RegexMaker.py

--- FinalProject/MannagerZvi.py
+++ FinalProject/MannagerZvi.py
@@ -1,5 +1,5 @@
 import DropBoxDownloader
-import RegexMaker
+import RegexMakerZvi
 from difflib import SequenceMatcher
 import scipy.stats
 import timeUnion
@@ -17,21 +17,42 @@ class MannagerZvi:
         self.srt_file_path = srt_file_path
         self.access_token = access_token
         self.dict_position = {}
+        self.rm = None
+        self.dict_word = {
+        }
 
-    def main_action(self, url_script, url_srt):
-        file_new = open("outputfilenew.txt", 'w')
+    def download_things(self, url_script, url_srt):
         d = DropBoxDownloader.DropBoxDownloader(self.access_token)
         d.download_file(url_script, self.script_file_path)
         d.download_file(url_srt, self.srt_file_path)
-        rm = RegexMaker.RegexMaker(self.script_file_path, self.regex_script, self.regex_srt, self.srt_file_path)
-        rm.find_matches_script()
-        rm.find_matches_srt()
-        normal_dis = scipy.stats.norm(1, 1.5).pdf
-        sumScript = sum(rm.dict_words_script.values())
-        sumSrt = sum(rm.dict_words_srt.values())
-        dict_word = {
+        self.rm = RegexMakerZvi.RegexMakerZvi(self.script_file_path, self.regex_script, self.regex_srt, self.srt_file_path)
+        self.rm.find_matches_script()
+        self.rm.find_matches_srt()
 
-        }
+    def before_main_action(self, a):
+        sumScript = sum(self.rm.dict_words_script.values())
+        sumSrt = sum(self.rm.dict_words_srt.values())
+        for lineScriptIndex in tqdm(range(len(self.rm.arr_words_script))):
+            lineScript = self.rm.arr_words_script[lineScriptIndex]
+            for wordIndex in range(len(lineScript.split(' '))):
+                line_word_matrix = numpy.zeros((1, len(self.rm.arr_words_srt)))
+                word = lineScript.split(' ')[wordIndex]
+                if word in self.dict_word.keys():
+                    pass
+                else:
+                    for lineSrtIndex in range(len(self.rm.arr_words_srt)):
+                        lineSrt = self.rm.arr_words_srt[lineSrtIndex]
+                        if word in lineSrt.split(' '):
+                            if word == "I":
+                                pass
+                            if lineSrt and lineScript:
+                                line_word_matrix[0][lineSrtIndex] = float((sumSrt - a*self.rm.dict_words_srt[word])) / float(
+                                    sumSrt) * float((sumScript - a*self.rm.dict_words_script[word])) / float(sumScript*len(lineScript.split(' ')))
+                    self.dict_word[word] = line_word_matrix
+
+    def main_action(self):
+        file_new = open("outputfilenew.txt", 'w')
+        normal_dis = scipy.stats.norm(1, 1.5).pdf
         cutoff_ratio = 0.05
         dict_lines = {
 
@@ -39,40 +60,25 @@ class MannagerZvi:
         bestTimeArr = []
         LastResult = 0
         ArrayRatios = []
-        for lineScriptIndex in tqdm(range(len(rm.arr_words_script))):
-            lineScript = rm.arr_words_script[lineScriptIndex]
-            for wordIndex in range(len(lineScript.split(' '))):
-                line_word_matrix = numpy.zeros((1, len(rm.arr_words_srt)))
-                word = lineScript.split(' ')[wordIndex]
-                if word in dict_word.keys():
-                    pass
-                else:
-                    for lineSrtIndex in range(len(rm.arr_words_srt)):
-                        lineSrt = rm.arr_words_srt[lineSrtIndex]
-                        if word in lineSrt.split(' '):
-                            if word == "I":
-                                pass
-                            if lineSrt and lineScript:
-                                line_word_matrix[0][lineSrtIndex] = float((sumSrt - 5*rm.dict_words_srt[word])) / float(
-                                    sumSrt) * float((sumScript - 5*rm.dict_words_script[word])) / float(sumScript*len(lineScript.split(' ')))
-                    dict_word[word] = line_word_matrix
-        for lineScriptIndex in tqdm(range(len(rm.arr_words_script))):
-            lineScript = rm.arr_words_script[lineScriptIndex]
-            line_matrix = dict_word[lineScript.split(' ')[0]]
+        for lineScriptIndex in tqdm(range(len(self.rm.arr_words_script))):
+            lineScript = self.rm.arr_words_script[lineScriptIndex]
+            if "Okay" in lineScript:
+                pass
+            line_matrix = self.dict_word[lineScript.split(' ')[0]]
             for wordIndex in range(len(lineScript.split(' ')) - 1):
                 if wordIndex == 0:
                     wordIndex += 1
                 word = lineScript.split(' ')[wordIndex]
-                line_matrix = numpy.row_stack((line_matrix, dict_word[word]))
+                line_matrix = numpy.row_stack((line_matrix, self.dict_word[word]))
             array_winners = numpy.dot(numpy.ones((1, len(lineScript.split(" ")))), line_matrix)
             if max(array_winners[0]) < cutoff_ratio:
                 array_ratios = []
-                for lineSrtIndex in range(len(rm.arr_words_srt)):
-                    lineSrt = rm.arr_words_srt[lineSrtIndex]
+                for lineSrtIndex in range(len(self.rm.arr_words_srt)):
+                    lineSrt = self.rm.arr_words_srt[lineSrtIndex]
                     array_ratios.append(SequenceMatcher(None, lineSrt, lineScript).ratio() * normal_dis((1.0/(1 + abs((lineSrtIndex - LastResult))))))
-                dict_lines[rm.dict_without_punctuation_script[rm.arr_words_script[lineScriptIndex]]] = rm.dict_without_punctuation_srt[rm.arr_words_srt[array_ratios.index(max(array_ratios))]].rstrip().lstrip().lstrip("-")
+                dict_lines[self.rm.dict_without_punctuation_script[self.rm.arr_words_script[lineScriptIndex]]] = self.rm.dict_without_punctuation_srt[self.rm.arr_words_srt[array_ratios.index(max(array_ratios))]].rstrip().lstrip().lstrip("-")
                 LastResult = array_ratios.index(max(array_ratios))
-                bestTimeArr.append(timeUnion.timeUnion(rm.srt_time[rm.srt_words[rm.arr_words_srt[array_ratios.index(max(array_ratios))]]]))
+                bestTimeArr.append(timeUnion.timeUnion(self.rm.srt_time[min(self.rm.srt_words[self.rm.arr_words_srt[array_ratios.index(max(array_ratios))]], key=lambda t: abs(t-LastResult))]))
             else:
                 indexed = list(enumerate(array_winners[0]))
                 sorted_indexed = sorted(indexed, key=operator.itemgetter(1))
@@ -91,25 +97,25 @@ class MannagerZvi:
                 strings_with_punc = []
                 for i in range(len(xSorted)):
                     t.append((xSorted[i],))
-                    timeLine = timeUnion.timeUnion(rm.srt_time[rm.srt_words[rm.arr_words_srt[xSorted[i]]]])
-                    best_str = rm.arr_words_srt[xSorted[i]]
-                    best_str_with_punc = rm.dict_without_punctuation_srt[rm.arr_words_srt[xSorted[i]]].rstrip().lstrip().lstrip("-")
+                    timeLine = timeUnion.timeUnion(self.rm.srt_time[min(self.rm.srt_words[self.rm.arr_words_srt[xSorted[i]]], key=lambda t: abs(t-LastResult))])
+                    best_str = self.rm.arr_words_srt[xSorted[i]]
+                    best_str_with_punc = self.rm.dict_without_punctuation_srt[self.rm.arr_words_srt[xSorted[i]]].rstrip().lstrip().lstrip("-")
                     ki = -1
-                    while ki + xSorted[i] in range(len(rm.arr_words_srt)) and SequenceMatcher(None, best_str,
+                    while ki + xSorted[i] in range(len(self.rm.arr_words_srt)) and SequenceMatcher(None, best_str,
                                                                                               lineScript).ratio() < SequenceMatcher(
-                            None, rm.arr_words_srt[xSorted[i] + ki] + " " + best_str, lineScript).ratio():
-                        best_str = rm.arr_words_srt[xSorted[i] + ki] + " " + best_str
-                        best_str_with_punc = rm.dict_without_punctuation_srt[rm.arr_words_srt[xSorted[i] + ki]].rstrip().lstrip().lstrip("-") + " " + best_str_with_punc
+                            None, self.rm.arr_words_srt[xSorted[i] + ki] + " " + best_str, lineScript).ratio():
+                        best_str = self.rm.arr_words_srt[xSorted[i] + ki] + " " + best_str
+                        best_str_with_punc = self.rm.dict_without_punctuation_srt[self.rm.arr_words_srt[xSorted[i] + ki]].rstrip().lstrip().lstrip("-") + " " + best_str_with_punc
                         t[j] = t[j] + (xSorted[i] + ki,)
-                        timeLine = timeLine.union(timeUnion.timeUnion(rm.srt_time[rm.srt_words[rm.arr_words_srt[xSorted[i] + ki]]]))
+                        timeLine = timeLine.union(timeUnion.timeUnion(self.rm.srt_time[min(self.rm.srt_words[self.rm.arr_words_srt[xSorted[i] + ki]], key=lambda t:abs(t-LastResult))]))
                         ki -= 1
                     ki = 1
-                    while ki + xSorted[i] in range(len(rm.arr_words_srt)) and SequenceMatcher(None, best_str, lineScript).ratio() < SequenceMatcher(None, best_str + " " + rm.arr_words_srt[xSorted[i] + ki], lineScript).ratio():
-                        best_str = best_str + " " + rm.arr_words_srt[xSorted[i] + ki]
-                        best_str_with_punc = best_str_with_punc + " " + rm.dict_without_punctuation_srt[rm.arr_words_srt[xSorted[i] + ki]].rstrip().lstrip().lstrip("-")
+                    while ki + xSorted[i] in range(len(self.rm.arr_words_srt)) and SequenceMatcher(None, best_str, lineScript).ratio() < SequenceMatcher(None, best_str + " " + self.rm.arr_words_srt[xSorted[i] + ki], lineScript).ratio():
+                        best_str = best_str + " " + self.rm.arr_words_srt[xSorted[i] + ki]
+                        best_str_with_punc = best_str_with_punc + " " + self.rm.dict_without_punctuation_srt[self.rm.arr_words_srt[xSorted[i] + ki]].rstrip().lstrip().lstrip("-")
                         t[j] = t[j] + (xSorted[i] + ki,)
                         timeLine = timeLine.union(
-                            timeUnion.timeUnion(rm.srt_time[rm.srt_words[rm.arr_words_srt[xSorted[i] + ki]]]))
+                            timeUnion.timeUnion(self.rm.srt_time[min(self.rm.srt_words[self.rm.arr_words_srt[xSorted[i] + ki]], key=lambda t:abs(t-LastResult))]))
                         ki += 1
                     j = j + 1
                     ratio_str.append(SequenceMatcher(None, best_str, lineScript).ratio())
@@ -124,29 +130,45 @@ class MannagerZvi:
                         the_best_distance = normal_dis((1.0/(1 + abs((num[0] - LastResult)))))*ratio_str[numIndex]
                         the_best_index = numIndex
                 LastResult = the_best_index
-                dict_lines[rm.dict_without_punctuation_script[rm.arr_words_script[lineScriptIndex]]] = strings_with_punc[the_best_index]
+                dict_lines[self.rm.dict_without_punctuation_script[self.rm.arr_words_script[lineScriptIndex]]] = strings_with_punc[the_best_index]
                 bestTimeArr.append(timeArr[the_best_index])
-            lineScriptAfterPunc = rm.dict_without_punctuation_script[rm.arr_words_script[lineScriptIndex]].rstrip().lstrip()
-            lineSrtPredicted = dict_lines[rm.dict_without_punctuation_script[rm.arr_words_script[lineScriptIndex]]].rstrip().lstrip()
+            lineScriptAfterPunc = self.rm.dict_without_punctuation_script[self.rm.arr_words_script[lineScriptIndex]].rstrip().lstrip()
+            lineSrtPredicted = dict_lines[self.rm.dict_without_punctuation_script[self.rm.arr_words_script[lineScriptIndex]]].rstrip().lstrip()
             Customize_Ratio = max([SequenceMatcher(None, lineScriptAfterPunc, lineSrtPredicted).ratio(), SequenceMatcher(None, lineSrtPredicted, lineScriptAfterPunc).ratio()])
             if Customize_Ratio < 0.85:
                 array_ratios = []
-                for lineSrtIndex in range(len(rm.arr_words_srt)):
-                    lineSrt = rm.arr_words_srt[lineSrtIndex]
-                    array_ratios.append(SequenceMatcher(None, lineSrt, lineScript).ratio() * normal_dis((1.0/(1 + abs((lineSrtIndex - LastResult))))))
-                if SequenceMatcher(None, rm.dict_without_punctuation_script[rm.arr_words_script[lineScriptIndex]].rstrip().lstrip(), rm.dict_without_punctuation_srt[rm.arr_words_srt[array_ratios.index(max(array_ratios))]].rstrip().lstrip().lstrip("-")).ratio() > Customize_Ratio:
-                    file_new.write(str(lineScriptIndex + 1) + ". " + str(timeUnion.timeUnion(rm.srt_time[rm.srt_words[rm.arr_words_srt[array_ratios.index(max(array_ratios))]]])) + "\n" + rm.script_talkers[lineScriptIndex] + "\n" + rm.dict_without_punctuation_script[rm.arr_words_script[lineScriptIndex]].rstrip().lstrip() + " - Script" + "\n" + rm.dict_without_punctuation_srt[rm.arr_words_srt[array_ratios.index(max(array_ratios))]].rstrip().lstrip().lstrip("-") + " - Srt" + "\n")
-                    ArrayRatios.append(SequenceMatcher(None, rm.dict_without_punctuation_script[rm.arr_words_script[lineScriptIndex]].rstrip().lstrip(), rm.dict_without_punctuation_srt[rm.arr_words_srt[array_ratios.index(max(array_ratios))]].rstrip().lstrip().lstrip("-")).ratio())
+                for lineSrtIndex in range(len(self.rm.arr_words_srt)):
+                    lineSrt = self.rm.arr_words_srt[lineSrtIndex]
+                    array_ratios.append(SequenceMatcher(None, lineSrt, lineScript).ratio())
+                max_ratio = max(array_ratios)
+                max_ratio_index = array_ratios.index(max_ratio)
+                if max_ratio > Customize_Ratio:
+                    if max_ratio > 0.5:
+                        file_new.write(str(lineScriptIndex + 1) + ". " + str(timeUnion.timeUnion(self.rm.srt_time[min(self.rm.srt_words[self.rm.arr_words_srt[max_ratio_index]], key=lambda t: abs(t-LastResult))])) + "\n" + self.rm.script_talkers[lineScriptIndex] + "\n" + self.rm.dict_without_punctuation_script[self.rm.arr_words_script[lineScriptIndex]].rstrip().lstrip() + " - Script" + "\n" + self.rm.dict_without_punctuation_srt[self.rm.arr_words_srt[array_ratios.index(max(array_ratios))]].rstrip().lstrip().lstrip("-") + " - Srt" + "\n")
+                        ArrayRatios.append(SequenceMatcher(None, self.rm.dict_without_punctuation_script[self.rm.arr_words_script[lineScriptIndex]].rstrip().lstrip(), self.rm.dict_without_punctuation_srt[self.rm.arr_words_srt[max_ratio_index]].rstrip().lstrip().lstrip("-")).ratio())
+                    else:
+                        file_new.write(
+                            str(lineScriptIndex + 1) + ". " + "\n" + self.rm.script_talkers[
+                                lineScriptIndex] + "\n" + lineScriptAfterPunc + " - Script" + "\n" + "NO IN SRT!!!!" + " - Srt" + "\n")
                 else:
-                    file_new.write(
-                        str(lineScriptIndex + 1) + ". " + str(bestTimeArr[lineScriptIndex]) + "\n" + rm.script_talkers[
-                            lineScriptIndex] + "\n" + lineScriptAfterPunc + " - Script" + "\n" + lineSrtPredicted + " - Srt" + "\n")
-                    ArrayRatios.append(Customize_Ratio)
+                    if Customize_Ratio > 0.5:
+                        file_new.write(
+                            str(lineScriptIndex + 1) + ". " + str(bestTimeArr[lineScriptIndex]) + "\n" + self.rm.script_talkers[
+                                lineScriptIndex] + "\n" + lineScriptAfterPunc + " - Script" + "\n" + lineSrtPredicted + " - Srt" + "\n")
+                        ArrayRatios.append(Customize_Ratio)
+                    else:
+                        file_new.write(
+                            str(lineScriptIndex + 1) + ". " + "\n" + self.rm.script_talkers[
+                                lineScriptIndex] + "\n" + lineScriptAfterPunc + " - Script" + "\n" + "NO IN SRT!!!!" + " - Srt" + "\n")
+
+
             else:
-                file_new.write(str(lineScriptIndex + 1) + ". " + str(bestTimeArr[lineScriptIndex]) + "\n" + rm.script_talkers[lineScriptIndex] + "\n" + lineScriptAfterPunc + " - Script" + "\n" + lineSrtPredicted + " - Srt" + "\n")
+                file_new.write(str(lineScriptIndex + 1) + ". " + str(bestTimeArr[lineScriptIndex]) + "\n" + self.rm.script_talkers[lineScriptIndex] + "\n" + lineScriptAfterPunc + " - Script" + "\n" + lineSrtPredicted + " - Srt" + "\n")
                 ArrayRatios.append(Customize_Ratio)
 
-        print sum(ArrayRatios)/len(ArrayRatios)
+        return sum(ArrayRatios)/len(ArrayRatios)
 
 d = MannagerZvi("script.txt", r"(.*):(.*)", r"\d\r\n(.*?)\r\n(.*?)\r\n\r\n", "srt.txt",constants.access_token)
-d.main_action(constants.url_script, constants.url_srt)
+d.download_things(constants.url_script, constants.url_srt)
+d.before_main_action(5)
+print d.main_action()
