b89a684e5a14e9af82c5d7abb05683b57ba0d342
omersk <omersk61@gmail.com>
2019-11-04 13:31:45 +0200

    FinalProject

--- FinalProject/constants.py
+++ FinalProject/constants.py
@@ -1,4 +1,4 @@
 url_srt = "/srt-scrip/srt-all/The_Big_Bang_Theory - season1.en/The Big Bang Theory - 1x01 - Pilot.HDTV.XOR.en.srt"
 url_script = "/srt-scrip/scripts/series-1-episode-1-pilot-episode.txt"
-access_token = "ZjLsrohKF6AAAAAAAAAA98V4yC0AX1uSLO7pCrQvgPEHTSyDuggV8GMoTiNH_lqh"
+access_token = "ZjLsrohKF6AAAAAAAAAA_PqACcyyEptWXVL8wUjxzpGAF_Iq3cNzTotinEsA8880"
 BIG_NUM = 10
