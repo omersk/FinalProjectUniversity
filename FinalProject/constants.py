e5a6987f630e722f02a1e02ce225364fb7c6eb9a
University <omersk61@gmail.com>
2019-11-11 13:13:04 +0200

    AccessToken

--- FinalProject/constants.py
+++ FinalProject/constants.py
@@ -9,5 +9,5 @@ for pkg in modules:
 
 url_srt = "/srt-scrip/srt-all/The_Big_Bang_Theory - season1.en/The Big Bang Theory - 1x01 - Pilot.HDTV.XOR.en.srt"
 url_script = "/srt-scrip/scripts/series-1-episode-1-pilot-episode.txt"
-access_token = "ZjLsrohKF6AAAAAAAAAA_PqACcyyEptWXVL8wUjxzpGAF_Iq3cNzTotinEsA8880"
+access_token = "ZjLsrohKF6AAAAAAAAABI5URO-nBYnGcRP1nuYMasHM7t3EiB7Su2psz9Ef1wuqG"
 BIG_NUM = 10
