94e628ab163a42ef53602498c7b0f6f04fb5b962
omersk <omersk@walla.co.il>
2019-11-17 22:03:10 +0200

    Improved The M.L. This reverts commit a36552cd2b25e81c963f1b55be5f0aafa55b06dd.
    # Conflicts:
    #	FinalProject/RegexMaker.py

--- FinalProject/ML.py
+++ FinalProject/ML.py
@@ -2,68 +2,75 @@ from sklearn.neural_network import MLPClassifier
 import python_speech_features
 import scipy.io.wavfile as wav
 from tqdm import tqdm
-
+import numpy as np
 from os import listdir
 from os.path import isfile, join
 import sys
+from random import shuffle
+import matplotlib.pyplot as plt
 
-
-class NullWriter(object):
-    def write(self, arg):
-        pass
-
-nullwrite = NullWriter()
-oldstdout = sys.stdout
-
-dict_file_mfcc = {}  # for every file it create the following dict : { mfcc_13_coeffs_of_file : speaker }
-dict_file_output = {}  # for every speaker it create vector, example { penny: [1,0,0,0], leonard: [0,0,1,0] }
-
-onlyfiles = [f for f in listdir("FinalAudios/") if isfile(join("FinalAudios/", f))]
-#num = raw_input("Number Between 0 and " + str(len(onlyfiles)) + " : ")
-i = 0
-for num in tqdm(range(len(onlyfiles))):
-    for f in onlyfiles:
-        if f != onlyfiles[int(num)]:
-            f_speaker = f.split("_")[0]
-            (rate, sig) = wav.read("FinalAudios/" + f)
-            try:
-                mfcc_feat = python_speech_features.mfcc(sig, rate)  # mfcc coeffs
-                dict_file_mfcc[tuple(mfcc_feat[0])] = f_speaker  # first 13 mfcc coeffs
-            except IndexError:
-                pass
-    j = 0
-
-    #print list(dict.fromkeys(dict_file_mfcc.values()))
-    our_output_vector = [0] * len(list(dict.fromkeys(dict_file_mfcc.values())))  # zero array
-    save_our_output_vector = [] + our_output_vector  # make it array with one in the speaker number position
-    for name in list(dict.fromkeys(dict_file_mfcc.values())):
-        our_output_vector = [] + save_our_output_vector
-        our_output_vector[j] = 1
-        dict_file_output[name] = our_output_vector  # output value for the speaker
-        j += 1
-
+winner = []
+for testNum in range(20):
     X = []
     Y = []
-    for x in dict_file_mfcc.keys():
-        X.append(list(x))
-        Y.append(dict_file_output[dict_file_mfcc[x]])
+    onlyfiles = [f for f in listdir("FinalAudios/") if isfile(join("FinalAudios/", f))]
+    shuffle(onlyfiles)
+    names = []
+    for file in onlyfiles:
+        if " " not in file.split("_")[0]:
+            names.append(file.split("_")[0])
+        else:
+            names.append(file.split("_")[0].split(" ")[0])
+    names = list(dict.fromkeys(names))
+    vector_names = []
+    i = 0
+    vector_for_each_name = [0] * len(names)
+    for name in names:
+        vector_for_each_name[i] += 1
+        vector_names.append(np.array(vector_for_each_name))
+        vector_for_each_name[i] -= 1
+        i += 1
+    for f in onlyfiles:
+        if " " not in f.split("_")[0]:
+            f_speaker = f.split("_")[0]
+        else:
+            f_speaker = f.split("_")[0].split(" ")[0]
+        (rate, sig) = wav.read("FinalAudios/" + f)
+        try:
+            mfcc_feat = python_speech_features.mfcc(sig, rate, winlen=0.2)  # mfcc coeffs
+            for index in range(len(mfcc_feat)):
+                X.append(np.array(mfcc_feat[index]))
+                Y.append(np.array(vector_names[names.index(f_speaker)]))
+        except IndexError:
+            pass
+    X = np.asarray(X)
+    Y = np.asarray(Y)
+    Y_test = Y[:50]
+    X_test = X[:50]
+    X = X[50:]
+    Y = Y[50:]
+
     clf = MLPClassifier(solver='lbfgs', alpha=1e-2, hidden_layer_sizes=(5, 3), random_state=2)  # create the NN
     clf.fit(X, Y)  # Train it
-    (rate, sig) = wav.read("FinalAudios/" + onlyfiles[int(num)])
-    #print onlyfiles[int(num)].split("_")[0]
-    mfcc_feat = python_speech_features.mfcc(sig, rate)
-    #print dict_file_output
-    winners = list(clf.predict_proba([list(mfcc_feat[0])]))
-    #print winners
-    dict_winner = {0: "Penny", 1: "Leonard", 2: "Sheldon", 3: "Receptionist"}
-    winner = dict_winner[list(winners[0]).index(max(winners[0]))]
-    print winner
-    if winner == onlyfiles[int(num)].split("_")[0]:
-        i += 1
-        #print "WE WON!!!!!"
-    else:
-        pass
-        #print "WE LOST!!!!!!"
-    sys.stdout = oldstdout  # enable output
 
-print i * 1.0 / len(onlyfiles)
+    for sample in range(len(X_test)):
+        if list(clf.predict([X[sample]])[0]) == list(Y_test[sample]):
+            winner.append(1)
+        else:
+            winner.append(0)
+
+plot_x = []
+plot_y = []
+for i in range(1, len(winner)):
+    plot_y.append(sum(winner[0:i])*1.0/len(winner[0:i]))
+    plot_x.append(i)
+plt.plot(plot_x, plot_y)
+plt.xlabel('x - axis')
+# naming the y axis
+plt.ylabel('y - axis')
+
+# giving a title to my graph
+plt.title('My first graph!')
+
+# function to show the plot
+plt.show()
