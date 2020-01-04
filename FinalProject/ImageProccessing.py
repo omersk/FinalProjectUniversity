50b90130127a94cec8e9ab0f64f9c5e4b671f8df
omersk <omersk@walla.co.il>
2020-01-04 16:11:42 +0200

    Merge remote-tracking branch 'origin/FinalProjectUniversity' into FinalProjectUniversity
    # Conflicts:
    #	FinalProject/constants.py
    #	FinalProject/outputfilenew.txt

--- FinalProject/ImageProccessing.py
+++ FinalProject/ImageProccessing.py
@@ -20,99 +20,105 @@ def main_action():
     by the script and srt files. And remove laugh + silent audios.
     :return:
     """
-    if not os.path.exists("Images"):  # if the dir doesn't exist we create one
-        os.makedirs("Images")
+    if not os.path.exists("Images2"):  # if the dir doesn't exist we create one
+        os.makedirs("Images2")
     num = str(input("espiode number : "))
     filename = "outputfilenew_" + num + ".txt"
     i = 1
     j = 1
     cutted_last = 0  # how much we cut last file
     while True:
-        print(i)
         if not linecache.getline(filename, i + 4):  # end of file
             break
         time = linecache.getline(filename, i).split(".")[1].lstrip().rstrip()  # time of the specific line
         last_image = 1
         if not time == "00:00:00,000 --> 00:00:00,000":  # if it's in the srt
-            try:
-                name = linecache.getline(filename, i + 1).lstrip().rstrip()  # name of the speaker
-                words = linecache.getline(filename, i + 3).lstrip().rstrip()  # what he said
-                t = timeUnion.timeUnion(time)
-                start_time = t.getInitialTimeInSec()  # start time
-                end_time = t.getEndTimeInSec()  # end time
-                if time == linecache.getline(filename, i + 4).split(".")[1].lstrip().rstrip():  # if there is 2
-                    i += 4  # next speaker
-                    j += 1  # next speaker
-                else:
-                    breaked = False
-                    # PreCheck
-                    vidcap = cv2.VideoCapture('Movie_' + str(num) + '.mp4')
-                    for second in np.linspace(start_time + 0.5, end_time - 0.5, 10):
+            name = linecache.getline(filename, i + 1).lstrip().rstrip()  # name of the speaker
+            words = linecache.getline(filename, i + 3).lstrip().rstrip()  # what he said
+            print(time)
+            print(name)
+            t = timeUnion.timeUnion(time)
+            start_time = t.getInitialTimeInSec()  # start time
+            end_time = t.getEndTimeInSec()  # end time
+            if time == linecache.getline(filename, i + 4).split(".")[1].lstrip().rstrip():  # if there is 2
+                i += 4  # next speaker
+                j += 1  # next speaker
+            else:
+                breaked = False
+                # PreCheck
+                vidcap = cv2.VideoCapture('Movie_' + str(num) + '.mp4')
+                for second in np.linspace(start_time + 0.5, end_time - 0.5, 10):
+                    vidcap.set(cv2.CAP_PROP_POS_MSEC, int(second * 1000))  # just cue to 20 sec. position
+                    success, image = vidcap.read()
+                    print(image.shape)
+                    if success:
+                        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                        if type(last_image) == int:
+                            last_image = gray
+                        (score, diff) = compare_ssim(gray, last_image, full=True)
+                        #                        print("SSIM: {}".format(score))
+                        if score < 0.4:
+                            breaked = True
+                            print(name, int(second * 1000))
+                            print("------------------- BREAKED -------------------")
+                            break
+                if not breaked:
+                    face_crop = []
+                    for second in np.linspace(start_time + 0.5, end_time - 0.5, (end_time - start_time) * 10):
+                        #                        print(name, int(second * 1000))
                         vidcap.set(cv2.CAP_PROP_POS_MSEC, int(second * 1000))  # just cue to 20 sec. position
                         success, image = vidcap.read()
                         if success:
+                            face_cascade = cv2.CascadeClassifier(
+                                "C:/Users/omer/MainProject/Lib/site-packages/cv2/data/haarcascade_profileface.xml")
                             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                            gray = gray[0:380, 0:2400]
+                            image = image[0:380, 0:2400]
+                            fliped = cv2.flip(gray, 1)
+                            fliped_image = cv2.flip(image, 1)
+                            faces1 = face_cascade.detectMultiScale(gray, 1.3, 5)
+                            faces2 = face_cascade.detectMultiScale(fliped, 1.3, 5)
+                            count_faces = str(len(faces1))
                             if type(last_image) == int:
                                 last_image = gray
-                            (score, diff) = compare_ssim(gray, last_image, full=True)
-                            #                        print("SSIM: {}".format(score))
-                            if score < 0.4:
-                                breaked = True
-                                break
-                    if not breaked:
-                        face_crop = []
-                        for second in np.linspace(start_time + 0.5, end_time - 0.5, (end_time - start_time) * 10):
-                            #                        print(name, int(second * 1000))
-                            vidcap.set(cv2.CAP_PROP_POS_MSEC, int(second * 1000))  # just cue to 20 sec. position
-                            success, image = vidcap.read()
-                            if success:
-                                face_cascade = cv2.CascadeClassifier(
-                                    "C:/Users/omer/MainProject/Lib/site-packages/cv2/data/haarcascade_profileface.xml")
-                                gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-                                gray = gray[0:380, 0:2400]
-                                image = image[0:380, 0:2400]
-                                fliped = cv2.flip(gray, 1)
-                                fliped_image = cv2.flip(image, 1)
-                                faces1 = face_cascade.detectMultiScale(gray, 1.3, 5)
-                                faces2 = face_cascade.detectMultiScale(fliped, 1.3, 5)
-                                count_faces = str(len(faces1))
-                                if type(last_image) == int:
-                                    last_image = gray
-                                for f in faces1:
-                                    x, y, w, h = [v for v in f]
-                                    cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 3)
-                                    # Define the region of interest in the image
-                                    face_crop.append(image[y:y + h, x:x + w])
-                                for f in faces2:
-                                    x, y, w, h = [v for v in f]
-                                    cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 3)
-                                    # Define the region of interest in the image
-                                    face_crop.append(fliped_image[y:y + h, x:x + w])
+                            for f in faces1:
+                                x, y, w, h = [v for v in f]
+                                cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 3)
+                                # Define the region of interest in the image
+                                face_crop.append(image[y:y + h, x:x + w])
+                                print("append to face_crop")
+                            for f in faces2:
+                                x, y, w, h = [v for v in f]
+                                cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 3)
+                                # Define the region of interest in the image
+                                face_crop.append(cv2.flip(fliped_image[y:y + h, x:x + w], 1))
+                                print("append to face_crop")
 
-                        wasBreaked = False
-                        min_1, min_2 = (500, 500)
+                    print(len(face_crop))
+                    wasBreaked = False
+                    min_1, min_2 = (500, 500)
+                    for face in face_crop:
+                        if face.shape[0] < min_1:
+                            min_1 = face.shape[0]
+                        if face.shape[1] < min_2:
+                            min_2 = face.shape[1]
+                    for face_i in range(0, len(face_crop) - 1):
+                        face = face_crop[face_i]
+                        face_next = face_crop[face_i + 1]
+                        print(face_next.shape, face.shape)
+                        (score, diff) = compare_ssim(face[:min_1, :min_2], face_next[:min_1, :min_2], full=True,
+                                                     multichannel=True)
+                        if score < 0.4:
+                            print(score)
+                            wasBreaked = True
+                            break
+                    if not wasBreaked:
+                        x = 0
                         for face in face_crop:
-                            if face.shape[0] < min_1:
-                                min_1 = face.shape[0]
-                            if face.shape[1] < min_2:
-                                min_2 = face.shape[1]
-                        for face_i in range(0, len(face_crop) - 1):
-                            face = face_crop[face_i]
-                            face_next = face_crop[face_i + 1]
-                            (score, diff) = compare_ssim(face[:min_1, :min_2], face_next[:min_1, :min_2], full=True,
-                                                         multichannel=True)
-                            if score < 0.4:
-                                wasBreaked = True
-                                break
-                        if not wasBreaked:
-                            x = 0
-                            for face in face_crop:
-                                x = x + 1
-                                cv2.imwrite(
-                                    "Images\\" + name + "_" + num + "_" + str(int((second + x) * 1000) / 1000) + ".jpg",
-                                    face)  # save frame as JPEG file
-            except:
-                pass
+                            x = x + 1
+                            cv2.imwrite(
+                                "Images2\\" + name + "_" + num + "_" + str(int((second + x) * 1000) / 1000) + ".jpg",
+                                face)  # save frame as JPEG file
         i += 4  # next speaker
         j += 1  # next speaker
 
--- FinalProject/ML.py
+++ FinalProject/ML.py
@@ -27,7 +27,7 @@ for TestNum in tqdm(range(40)):  # We check it several times
     if not outfile_x:  # if path not exist we create it
         X = []  # inputs
         Y = []  # outputs
-        onlyfiles = [f for f in listdir("FinalAudios") if isfile(join("FinalAudios", f))]   # Files in dir
+        onlyfiles = [f for f in listdir("FinalAudios1") if isfile(join("FinalAudios1", f))]   # Files in dir
         names = []  # names of the speakers
         for file in onlyfiles:  # for each wav sound
             # UNESSECERY TO UNDERSTAND THE CODE
@@ -39,7 +39,7 @@ for TestNum in tqdm(range(40)):  # We check it several times
         namesWithoutDuplicate = list(dict.fromkeys(names))
         namesWithoutDuplicateCopy = namesWithoutDuplicate[:]
         for name in namesWithoutDuplicateCopy:  # we remove low samples files
-            if names.count(name) < 2:
+            if names.count(name) < 1:
                 namesWithoutDuplicate.remove(name)
         names = namesWithoutDuplicate
         print(names)  # print it
@@ -55,7 +55,7 @@ for TestNum in tqdm(range(40)):  # We check it several times
             else:
                 f_speaker = f.split("_")[0].split(" ")[0]
             if f_speaker in namesWithoutDuplicate:
-                fs, audio = wav.read("FinalAudios\\" + f)  # read the file
+                fs, audio = wav.read("FinalAudios1\\" + f)  # read the file
                 try:
                     # compute MFCC
                     mfcc_feat = python_speech_features.mfcc(audio, samplerate=fs, winlen=win_len, winstep=step, nfft=nfft, appendEnergy=False)
@@ -69,7 +69,7 @@ for TestNum in tqdm(range(40)):  # We check it several times
             else:
                 if not os.path.exists("TooLowSamples"):  # if path not exist we create it
                     os.makedirs("TooLowSamples")
-                shutil.move("FinalAudios\\" + f, "TooLowSamples\\" + f)
+                shutil.move("FinalAudios1\\" + f, "TooLowSamples\\" + f)
         outfile_x = TemporaryFile()
         np.save(outfile_x, X)
         outfile_y = TemporaryFile()
@@ -90,14 +90,14 @@ for TestNum in tqdm(range(40)):  # We check it several times
     Y = list(Y)
     lenX = len(X)
     # ------------------- RANDOMIZATION, UNNECESSARY TO UNDERSTAND THE CODE ------------------- #
-    y_test = np.asarray(Y[:100])   # CHOOSE 100 FOR TEST, OTHERS FOR TRAIN
-    x_test = np.asarray(X[:100])   # CHOOSE 100 FOR TEST, OTHERS FOR TRAIN
-    x_train = np.asarray(X[100:])  # CHOOSE 100 FOR TEST, OTHERS FOR TRAIN
-    y_train = np.asarray(Y[100:])  # CHOOSE 100 FOR TEST, OTHERS FOR TRAIN
-    x_val = x_train[-100:]         # FROM THE TRAIN CHOOSE 100 FOR VALIDATION
-    y_val = y_train[-100:]         # FROM THE TRAIN CHOOSE 100 FOR VALIDATION
-    x_train = x_train[:-100]       # FROM THE TRAIN CHOOSE 100 FOR VALIDATION
-    y_train = y_train[:-100]       # FROM THE TRAIN CHOOSE 100 FOR VALIDATION
+    y_test = np.asarray(Y[:1000])   # CHOOSE 100 FOR TEST, OTHERS FOR TRAIN
+    x_test = np.asarray(X[:1000])   # CHOOSE 100 FOR TEST, OTHERS FOR TRAIN
+    x_train = np.asarray(X[1000:])  # CHOOSE 100 FOR TEST, OTHERS FOR TRAIN
+    y_train = np.asarray(Y[1000:])  # CHOOSE 100 FOR TEST, OTHERS FOR TRAIN
+    x_val = x_train[-1000:]         # FROM THE TRAIN CHOOSE 100 FOR VALIDATION
+    y_val = y_train[-1000:]         # FROM THE TRAIN CHOOSE 100 FOR VALIDATION
+    x_train = x_train[:-1000]       # FROM THE TRAIN CHOOSE 100 FOR VALIDATION
+    y_train = y_train[:-1000]       # FROM THE TRAIN CHOOSE 100 FOR VALIDATION
     x_train = x_train.reshape(np.append(x_train.shape, 1))  # RESHAPE FOR INPUT
     x_test = x_test.reshape(np.append(x_test.shape, 1))     # RESHAPE FOR INPUT
     x_val = x_val.reshape(np.append(x_val.shape, 1))        # RESHAPE FOR INPUT
@@ -117,7 +117,7 @@ for TestNum in tqdm(range(40)):  # We check it several times
     # -------------- OUR TENSOR FLOW NEURAL NETWORK MODEL -------------- #
 
     print("fitting")
-    history = model.fit(x_train, y_train, epochs=2, validation_data=(x_val, y_val))
+    history = model.fit(x_train, y_train, epochs=15, validation_data=(x_val, y_val))
     print("testing")
     results.append(model.evaluate(x_test, y_test)[1])
     print(results)
@@ -144,3 +144,41 @@ for TestNum in tqdm(range(40)):  # We check it several times
     #pyplot.plot(history.history['val_loss'], label='test')                                          Q
     #pyplot.legend()
     #pyplot.show()
+
+#num = []
+#for id in range(len(testFiles)):
+#    fs, audio = wav.read("FinalAudios2\\" + testFiles[id])  # read the file
+#    mfcc_feat = python_speech_features.mfcc(audio, samplerate=fs, winlen=win_len, winstep=step, nfft=nfft,
+#                                   appendEnergy=False)
+#    sheldon0 = 0
+#    leonard0 = 0
+#    penny0 = 0
+#    sheldon1 = 0
+#    leonard1 = 0
+#    penny1 = 0
+#    for i in range(len(mfcc_feat)):
+#        x_now = mfcc_feat[i]
+#        x1 = list(model1.predict_proba(x_now.reshape(1, 13, 1, 1))[0])
+#        x2 = list(model2.predict_proba(x_now.reshape(1, 13, 1, 1))[0])
+#        x3 = list(model3.predict_proba(x_now.reshape(1, 13, 1, 1))[0])
+#        sheldon0 = sheldon0 + x1[1]
+#        sheldon1 = sheldon1 + x2[1]
+#        leonard0 = leonard0 + x1[0]
+#        leonard1 = leonard1 + x3[0]
+#        penny0 = penny0 + x2[0]
+#        penny1 = penny1 + x3[1]
+#    if (max([sheldon0, sheldon1, leonard0, leonard1, penny0, penny1]) == penny0 or max([sheldon0, sheldon1, leonard0, leonard1, penny0, penny1]) == penny1):
+#        decide = 'Penny'
+#    elif (max([sheldon0, sheldon1, leonard0, leonard1, penny0, penny1]) == sheldon0 or max([sheldon0, sheldon1, leonard0, leonard1, penny0, penny1]) == sheldon1):
+#        decide = 'Sheldon'
+#    else:
+#        decide = 'Leonard'
+#    if decide == testFiles[id].split('_')[0]:
+#        num.append(1)
+#    else:
+#        num.append(0)
+#    print(sum(num)/len(num))
+#
+#
+#
+#
